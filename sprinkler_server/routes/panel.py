d13166e9b0467d8201bedc53ea767fd89291f3e1
Random121 <davidliyk@gmail.com>
2022-07-27 19:24:41 -0600

    made frontend dynamically get endpoint

--- sprinkler_server/routes/panel.py
+++ sprinkler_server/routes/panel.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 
+from sprinkler_server.utils import get_lan_ip_address
+
 panel_blueprint = Blueprint(
     "control_panel",
     __name__,
@@ -16,7 +18,10 @@ def default_page():
 
 @panel_blueprint.route("/control")
 def control_page():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        server_host=get_lan_ip_address(),
+    )
 
 
 @panel_blueprint.route("/schedule")
